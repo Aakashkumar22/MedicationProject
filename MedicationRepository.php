f9017e10f5713b34e014cc155b2b76b9aeebc57e
Aakash kumar <aakashakay222@gmail.com>
2025-10-01 23:04:19 +0530

    Optimzed Query Fetching from database

--- MedicationRepository.php
+++ MedicationRepository.php
@@ -176,58 +176,78 @@ class MedicationRepository
     // Load medication structure from database
     public function getMedicationById($id)
     {
-        $medication = new Medication($id);
+        $query = "
+        SELECT 
+            m.id as medication_id,
+            m.created_at,
+            m.updated_at,
+            mc.id as class_id,
+            co.id as object_id,
+            co.object_name as class_name,
+            ad.id as drug_id,
+            ad.drug_type,
+            ad.name as drug_name,
+            ad.strength,
+            ad.dose
+        FROM medications m
+        LEFT JOIN medication_classes mc ON m.id = mc.medication_id
+        LEFT JOIN class_objects co ON mc.id = co.medication_class_id
+        LEFT JOIN associated_drugs ad ON co.id = ad.class_object_id
+        WHERE m.id = ?
+        ORDER BY mc.id, co.id, ad.drug_type
+    ";
 
-        // Get medication classes
-        $stmt = $this->pdo->prepare("SELECT * FROM medication_classes WHERE medication_id = ?");
+        $stmt = $this->pdo->prepare($query);
         $stmt->execute([$id]);
-        $classes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($classes as $class) {
-            $medClass = new MedicationClass($class['id']);
-
-            // Get class objects grouped by className (object_name)
-            $stmt = $this->pdo->prepare("
-                SELECT object_name, GROUP_CONCAT(id) as object_ids 
-                FROM class_objects 
-                WHERE medication_class_id = ? 
-                GROUP BY object_name
-            ");
-            $stmt->execute([$class['id']]);
-            $objectGroups = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($objectGroups as $group) {
-                $className = $group['object_name'];
-                $objectIds = explode(',', $group['object_ids']);
-                $classObjects = [];
-
-                foreach ($objectIds as $objectId) {
-                    $classObj = new ClassObject($objectId);
-
-                    // Get associated drugs for this class object
-                    $stmt = $this->pdo->prepare("SELECT * FROM associated_drugs WHERE class_object_id = ?");
-                    $stmt->execute([$objectId]);
-                    $drugs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                    foreach ($drugs as $drug) {
-                        $associatedDrug = new AssociatedDrug(
-                            $drug['name'],
-                            $drug['strength'],
-                            $drug['dose'],
-                            $drug['id']
-                        );
-                        $classObj->addAssociatedDrug($drug['drug_type'], $associatedDrug);
-                    }
-
-                    $classObjects[] = $classObj;
-                }
-
-                $medClass->addClassName($className, $classObjects);
-            }
-
-            $medication->addMedicationClass($medClass);
+        if (empty($results)) {
+            return null;
         }
 
+        return $this->buildMedicationFromResults($results);
+    }
+
+    private function buildMedicationFromResults($results)
+    {
+        $medication = new Medication($results[0]['medication_id']);
+        $medClass = new MedicationClass();
+
+        $classObjects = [];
+
+        foreach ($results as $row) {
+            $className = $row['class_name'];
+
+            // Skip if no class name (shouldn't happen in valid data)
+            if (empty($className)) {
+                continue;
+            }
+
+            // Initialize class object if not exists
+            if (!isset($classObjects[$className])) {
+                $classObjects[$className] = new ClassObject($row['object_id']);
+            }
+
+            // Add drug to class object if drug data exists
+            if (!empty($row['drug_type']) && !empty($row['drug_name'])) {
+                $classObjects[$className]->addAssociatedDrug(
+                    $row['drug_type'],
+                    new AssociatedDrug(
+                        $row['drug_name'],
+                        $row['strength'],
+                        $row['dose'],
+                        $row['drug_id']
+                    )
+                );
+            }
+        }
+
+        // Add all class objects to medication class
+        foreach ($classObjects as $className => $classObj) {
+            $medClass->addClassName($className, [$classObj]);
+        }
+
+        $medication->addMedicationClass($medClass);
         return $medication;
     }
 
